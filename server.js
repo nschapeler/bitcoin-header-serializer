4673b3139c77e52b2d4b8dd0c8f96a658dbb8730
Nicolas Schapeler <38372048+nschapeler@users.noreply.github.com>
2020-07-16 17:17:48 +0200

    Comments

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ const bodyParser = require('body-parser');
 const request = require("request");
 
 const app = express()
+// For local execution
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+app.listen(port, function () {
+  console.log('App listening on port 3000!')
+})
+
+// Send the ejs view without any error or result on the first request
 app.get('/', function (req, res) {
   res.render('index', {serialized: null, err: null});
 })
 
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
-})
 
+// Reverse Endianess, as bitcoin wants little endian and the API provides big endian
 const reverseEndianess = (hexString => {
   const inversed = [];
   let i = hexString.length - 2;
@@ -28,7 +32,6 @@ const reverseEndianess = (hexString => {
 })
 
 const serialize = (data => {
-  // Need to reverse the endianess
   const versionHex = reverseEndianess(data['version'].toString(16));
   const prevBlockHash = reverseEndianess(data['prev_block_hash']);
   const mRoot = reverseEndianess(data['mrkl_root']);
@@ -46,8 +49,9 @@ app.post('/', function (req, res) {
   request.get(btcApiUrl, function(err, response, body){
     // Simple error handling
     if(err || response.statusCode != 200){
-      return res.render('index', {serialized: null, err: 'API error while fetching your block, please try again'});
+      res.render('index', {serialized: null, err: 'API error while fetching your block, please try again'});
     }
+    // Send back the serialized header of the block requested
     else {
       const bodyParsed = JSON.parse(body);
       res.render('index', {serialized: serialize(bodyParsed['data']), err: null});
